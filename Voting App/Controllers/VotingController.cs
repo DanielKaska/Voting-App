99223a4695c022398e71bb31550a24a026ea95a1
Daniel <daniel.kaska2004@gmail.com>
2023-12-03 12:48:37 +0100

    Dodaj pliki projektów.

---
+++ Voting App/Controllers/VotingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Voting_App.Controllers
+{
+    public class VotingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Voting App/Entities/Vote.cs
@@ -0,0 +1,11 @@
+﻿namespace Voting_App.Entities
+{
+    public class Vote
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual User CreatedBy { get; set; }
+    }
+}
