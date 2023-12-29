d9ca22002637bd2e238943cc470d7f54704713ca
Daniel <daniel.kaska2004@gmail.com>
2023-12-29 15:25:26 +0100

    model changes

---
+++ Voting App/Models/AnswerDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Voting_App.Models
+{
+    public class AnswerDto
+    {
+        public string Name { get; set; }
+    }
+}
--- Voting App/Models/VoteDto.cs
+++ Voting App/Models/VoteDto.cs
@@ -1,8 +1,16 @@
-﻿namespace Voting_App.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Voting_App.Entities;
+
+namespace Voting_App.Models
 {
     public class VoteDto
     {
+
         public string Name { get; set; }
+
         public string Description { get; set; }
+
+        public List<AnswerDto> Answers { get; set; }
+
     }
 }
