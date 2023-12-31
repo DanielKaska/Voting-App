83504cd15dff942575eb52e22d3df8ddf39374dd
Daniel <daniel.kaska2004@gmail.com>
2023-12-31 09:20:50 +0100

    database refactor

---
+++ Voting App/Migrations/20231231081706_delete-answerid-from-vote.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VotingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class deleteansweridfromvote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnswerId",
+                table: "votes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AnswerId",
+                table: "votes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
