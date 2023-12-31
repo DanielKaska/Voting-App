7606097eb3b3b7222a033bb0f566e056f0c93633
Daniel <daniel.kaska2004@gmail.com>
2023-12-31 09:52:32 +0100

    added custom exceptions

---
+++ Voting App/Exceptions/NotFoundException.cs
@@ -0,0 +1,21 @@
+﻿namespace Voting_App.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() 
+        { 
+        
+        }
+
+        public NotFoundException(string m) : base (m)
+        {
+
+        }
+
+        public NotFoundException(string m, Exception i) : base (m, i)
+        {
+
+        }
+
+    }
+}
